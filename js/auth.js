6410ea8c416e6fd89964ff18b2936ef4ed5df11b
Peterson Lima <peter.lima104dev@hotmail.com>
2026-01-17 19:32:23 -0300

    Correção de bug v6

--- js/auth.js
+++ js/auth.js
@@ -1,31 +1,33 @@
-import { auth } from "./firebase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import {
+  getAuth,
   signInWithEmailAndPassword,
   onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-console.log("auth.js carregado");
+import { app } from "./firebase.js";
 
-window.login = function () {
+const auth = getAuth(app);
+
+// LOGIN
+window.login = async function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  signInWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      alert("Login realizado com sucesso");
-      location.reload();
-
-      document.querySelectorAll(".admin-only").forEach((el) => {
-        el.style.display = "table-cell";
-      });
-    })
-    .catch((err) => {
-      alert("Erro no login");
-      console.error(err);
-    });
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    bootstrap.Modal.getInstance(document.getElementById("loginModal")).hide();
+  } catch (err) {
+    alert("Erro no login");
+    console.error(err);
+  }
 };
 
+// CONTROLE ADMIN
 onAuthStateChanged(auth, (user) => {
-  console.log("Usuário:", user);
-  document.body.classList.toggle("admin", !!user);
+  const adminEls = document.querySelectorAll(".admin-only");
+
+  adminEls.forEach((el) => {
+    el.style.display = user ? "inline-block" : "none";
+  });
 });
