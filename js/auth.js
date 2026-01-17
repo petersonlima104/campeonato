710c0182be674c0fe52dd9cab8d7c74c56b1eb6e
Peterson Lima <peter.lima104dev@hotmail.com>
2026-01-17 09:35:13 -0300

    Correção de bug

--- js/auth.js
+++ js/auth.js
@@ -4,12 +4,22 @@ import {
   onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-window.login = () => {
-  signInWithEmailAndPassword(auth, email.value, password.value)
-    .then(() => location.reload())
-    .catch(() => alert("Login inválido"));
+// 👇 EXPLICITAMENTE GLOBAL
+window.login = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      location.reload();
+    })
+    .catch((err) => {
+      alert("Email ou senha inválidos");
+      console.error(err);
+    });
 };
 
+// CONTROLE DE ESTADO
 onAuthStateChanged(auth, (user) => {
   document.body.classList.toggle("admin", !!user);
 });
