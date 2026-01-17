ec80ff5071b4b74cfedbfdce0b361935a931e5fb
Peterson Lima <peter.lima104dev@hotmail.com>
2026-01-17 19:40:54 -0300

    Correção de bug v8

--- js/auth.js
+++ js/auth.js
@@ -4,20 +4,21 @@ import {
   onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+console.log("auth.js carregado");
+
 window.login = async function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   try {
     await signInWithEmailAndPassword(auth, email, password);
-    bootstrap.Modal.getInstance(document.getElementById("loginModal")).hide();
-  } catch (err) {
+    alert("Login realizado");
+  } catch (e) {
     alert("Erro no login");
-    console.error(err);
+    console.error(e);
   }
 };
 
-// CONTROLE DE VISIBILIDADE ADMIN
 onAuthStateChanged(auth, (user) => {
   document.querySelectorAll(".admin-only").forEach((el) => {
     el.style.display = user ? "inline-block" : "none";
--- js/firebase.js
+++ js/firebase.js
@@ -12,6 +12,7 @@ const firebaseConfig = {
   measurementId: "G-X3VPJ251DZ",
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
